2f81587b5cf9d22be0c4b9c831eddef8442796b7
TheNightmareX <TheNightmareX@foxmail.com>
2022-01-07 22:35:58 +0800

    fix(module): import `MikroOrmModule`

--- src/mikro-flusher.module.ts
+++ src/mikro-flusher.module.ts
@@ -1,10 +1,11 @@
+import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
 
 import { MikroFlusherInterceptor } from "./mikro-flusher.interceptor";
 import { MikroFlusherContextMiddleware } from "./mikro-flusher-context.middleware";
-
 @Module({
+  imports: [MikroOrmModule.forFeature({})],
   providers: [
     {
       provide: APP_INTERCEPTOR,
